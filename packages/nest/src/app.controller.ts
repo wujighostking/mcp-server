534b3c6be1cf50256a1a2deecab7359e6720440e
gk <promise_ghostking@163.com>
2026-03-25 20:58:02 +0800

    chore: 暂时跑通网络连接 mcp

--- packages/nest/src/app.controller.ts
+++ packages/nest/src/app.controller.ts
@@ -1,12 +1,49 @@
 import { Controller, Get, Post, Req, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 
+import { StreamableHTTPServerTransport } from '@modelcontextprotocol/sdk/server/streamableHttp.js';
+import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
+import { z } from 'zod';
+import crypto from 'node:crypto';
+
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private readonly server = new McpServer({ name: 'stream', version: '1.0.0' });
+  private transport: StreamableHTTPServerTransport =
+    new StreamableHTTPServerTransport({
+      sessionIdGenerator: () => crypto.randomUUID(),
+    });
+
+  constructor(private readonly appService: AppService) {
+    this.server.registerTool(
+      'sum',
+      {
+        title: 'Sum',
+        description: '计算两数之和',
+        inputSchema: {
+          a: z.number().describe('第一个数字'),
+          b: z.number().describe('第二个数字'),
+        },
+      },
+      async ({ a, b }) => {
+        const sum = a + b;
+
+        return {
+          content: [
+            {
+              type: 'text',
+              text: sum.toString(),
+            },
+          ],
+        };
+      },
+    );
+
+    this.server.connect(this.transport);
+  }
 
   @Post()
   summary(@Req() req: any, @Res() res: any) {
-    return this.appService.summarize(req, res);
+    return this.transport.handleRequest(req, res, req.body); // res.send('hello world1') //this.appService.summarize(req, res);
   }
 }
--- packages/nest/src/app.service.ts
+++ packages/nest/src/app.service.ts
@@ -1,50 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import { StreamableHTTPServerTransport } from '@modelcontextprotocol/sdk/server/streamableHttp.js';
-import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
-import { z } from 'zod';
-import crypto from 'node:crypto';
 
 @Injectable()
 export class AppService {
-  private readonly server = new McpServer({ name: 'stream', version: '1.0.0' });
-  private transport: StreamableHTTPServerTransport =
-    new StreamableHTTPServerTransport({
-      sessionIdGenerator: () => crypto.randomUUID(),
-    });
+
 
   constructor() {
-    this.server.registerTool(
-      'sum',
-      {
-        title: 'Sum',
-        description: '计算两数之和',
-        inputSchema: {
-          a: z.number().describe('第一个数字'),
-          b: z.number().describe('第二个数字'),
-        },
-      },
-      async ({ a, b }) => {
-        const sum = a + b;
 
-        return {
-          content: [
-            {
-              type: 'text',
-              text: sum.toString(),
-            },
-          ],
-        };
-      },
-    );
   }
 
   async summarize(req: any, res: any) {
-    await this.server.connect(this.transport);
 
-    await this.transport.handleRequest(req, res, req.body);
-
-    this.transport.close();
-
-    return;
+    return
   }
 }
