4e1aaed815b61b8a347d52692dd60fd8ccb8647c
gk <promise_ghostking@163.com>
2026-03-29 00:02:36 +0800

    chore: 添加 sum 和 multiply 工具注册到 ServerRegister 类

--- packages/nest/src/server.register.ts
+++ packages/nest/src/server.register.ts
@@ -7,6 +7,11 @@ export class ServerRegister {
   }
 
   register() {
+    this.sumRegister()
+    this.multiplyRegister()
+  }
+
+  private sumRegister() {
     this.server.registerTool(
       'sum',
       {
@@ -25,6 +30,32 @@ export class ServerRegister {
             {
               type: 'text',
               text: sum.toString(),
+            },
+          ],
+        };
+      }
+    )
+  }
+
+  private multiplyRegister() {
+    this.server.registerTool(
+      'multiply',
+      {
+        title: 'Multiply',
+        description: '计算两数之积',
+        inputSchema: {
+          a: z.number().describe('第一个数字'),
+          b: z.number().describe('第二个数字'),
+        },
+      },
+      async ({ a, b }) => {
+        const product = a * b;
+
+        return {
+          content: [
+            {
+              type: 'text',
+              text: product.toString(),
             },
           ],
         };
